c3286f8ee7e93ddd648b6c94ad3a61a1e8e26309
Brayan Lopez <brayan9105@hotmail.com>
2022-07-31 20:51:15 -0500

    - Add user model test - Add sign in test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  describe 'valid factory' do
+    it { is_expected.to be_valid }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_presence_of :password }
+  end
 end
---
+++ spec/support/authentication.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Authentication
+	def sign_in_user(user)
+	end
+end
